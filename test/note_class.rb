6e0a8caacc032ff024daea964cfbbb74889fdaaf
Wade West <wwest81@gmail.com>
2010-11-03 18:40:32 -0500

    allow our NoteClass for testing to switch between good user info and bad user info.

--- test/note_class.rb
+++ test/note_class.rb
@@ -1,5 +1,16 @@
 require 'catch_notes'
 class NoteClass < CatchNotes::Base
-  username ENV['CATCH_USER']
-  password ENV['CATCH_PASS']
+  
+  def self.good_user
+    username ENV['CATCH_USER']
+    password ENV['CATCH_PASS']
+  end
+  
+  def self.bad_user
+    username "foo@example.com"
+    password "foobar"
+  end
+  
+  good_user
+  
 end
