2ee0c295b89d8beb51ba44e9257e785a37eb4ff8
Wade West <wwest81@gmail.com>
2010-11-03 16:24:10 -0500

    Cleaned up the AuthItems' code

--- lib/catch_notes/base.rb
+++ lib/catch_notes/base.rb
@@ -13,25 +13,27 @@ module CatchNotes
     # Methods for Authenication
     module AuthItems
       module ClassMethods
-        @@auth = {}
         def username( username )
-          send(:basic_auth, username, @@auth[:password] || '')
-          @@auth[:username] = username
+          send(:basic_auth, username, get_auth[:password] || '')
         end
     
         def password( password )
-          send(:basic_auth, @@auth[:username] || '', password)
-          @@auth[:password] = password
+          send(:basic_auth, get_auth[:username] || '', password)
         end
       
         def valid_username?
-          username = @@auth[:username]
+          username = get_auth[:username]
           !(username.nil? || username == '')
         end
       
         def valid_password?
-          password = @@auth[:password]
+          password = get_auth[:password]
           !(password.nil? || password == '')
+        end
+        
+        private
+        def get_auth
+          send(:default_options)[:basic_auth] || {}
         end
       end
       
