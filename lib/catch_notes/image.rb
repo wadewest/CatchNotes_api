fbb7d3a933581fb1486b079f3e19527cfb55c6b0
Wade West <wwest81@gmail.com>
2010-11-16 22:24:11 -0600

    Added some documentation to the Image class.

--- lib/catch_notes/image.rb
+++ lib/catch_notes/image.rb
@@ -16,12 +16,21 @@ module CatchNotes
       end
     end
     
+    # Sets the url of the image to +new_url+
+    #
+    # ==== Parameters
+    # * +new_url+ - A URL that points to an image.  The data attribute will be set to nil when this is set.
+    #
+    # ==== Example
+    #   image = CatchNotes::Image.new
+    #   image.url = 'http://example.com/hosted/image.jpg'
     def url=(new_url)
       @data = nil
       @url = new_url
     end
     
-    def data #:nodoc:
+    # Returns the raw data of the image.  If there is no data, it will be generated from the image at +url+ if it is set.
+    def data
       if @data.nil? && !@url.nil?
         res = HTTParty.get @url
         if res.code == 200
@@ -33,15 +42,23 @@ module CatchNotes
       @data
     end
     
+    # Set the raw data of the image to +new_data+.  The url attribute will be set to nil when this is set.
+    #
+    # ==== Parameters
+    # * +new_data+ - The data of an image.
+    #
+    # ==== Example
+    #   image = CatchNotes::Image.new
+    #   image.data = File.read('tmp/some_uploaded_image.jpg')
     def data=(new_data)
       @url = nil
       @data = new_data
     end
     
-    def destroy!
+    def destroy! #:nodoc:
     end
     
-    def destroy
+    def destroy #:nodoc:
       destroy!
     rescue
       false
