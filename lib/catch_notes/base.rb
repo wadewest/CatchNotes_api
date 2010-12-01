3c5da22f04beb785628d38633b9d769b46a3a6d1
Wade West <wwest81@gmail.com>
2010-12-01 10:57:32 -0600

    Tags should be readonly

--- lib/catch_notes/base.rb
+++ lib/catch_notes/base.rb
@@ -4,8 +4,8 @@ module CatchNotes
   class Base
     
     attr_reader :id, :created_at, :modified_at, :source, :source_url,
-      :children, :user_name, :user_id
-    attr_accessor :text, :summary, :tags, :reminder_at, :location
+      :children, :user_name, :user_id, :tags
+    attr_accessor :text, :summary, :reminder_at, :location
     
     include HTTParty
     base_uri "https://api.catch.com/v1"
